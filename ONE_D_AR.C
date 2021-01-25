83b39253f76020bcb0a6b4590bdfb45a8f670d73
Algorithmoflife-sys <76839616+Algorithmoflife-sys@users.noreply.github.com>
2021-01-25 09:37:55 -0800

    one dimensional Array
    program for one dimensional Array

---
+++ ONE_D_AR.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+
+	int marks[5]= {3,4,5,2,6};
+		     //0,1,2,3,4   0 to size-1 ==>0 to 4
+	clrscr();
+
+	printf("%d",marks[2]);
+	getch();
+}
